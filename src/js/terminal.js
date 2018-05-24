83d29ce4b173cc95d86179cf6a1124c2ddd96fd4
k <kevinmurphy678@gmail.com>
2018-05-24 04:19:46 -0400

    Initial commit

---
+++ src/js/terminal.js
@@ -0,0 +1,72 @@
+//https://codepen.io/uplusion23/pen/qbJVdg
+$('#rick').hide();
+
+
+var terminalElement = $(".terminal")[0];
+var dialogue	=	document.getElementsByClassName("dialogue")[0];
+1 || (function(o){
+  var open = false;
+  Object.defineProperty(o, "open", {
+    get:	function(){ return o.classList.contains("open"); },
+    set:	function(i){ o.classList.toggle("open", !!i); }
+  });
+}(dialogue));
+function snakeText(from, to, refreshRate, charAmount, autoScroll, endFunction){
+
+
+
+  var fromText, toText, l, i,
+    refreshRate		=	refreshRate	|| 20,
+    charAmount		=	charAmount	|| 1;
+  if(3 === from.nodeType) fromText = from;
+  else for(i = l = from.childNodes.length-1; i >= 0; --i)
+    if(3 === from.childNodes[i].nodeType){
+      fromText = from.childNodes[i];
+      break;
+    }
+  if(!fromText) throw new ArgumentError("Source object is neither a text node or element containing any text nodes.");
+  if(3 === to.nodeType) toText = to;
+  else for(i = l = to.childNodes.length-1; i >= 0; --i)
+    if(3 === to.childNodes[i].nodeType){
+      toText	=	to.childNodes[i];
+      break;
+    }
+
+  toText	=	toText || to.appendChild(document.createTextNode(""));
+  var interval = setInterval(function(){
+
+    var from	=	fromText.data;
+    if(!from.length) {
+      if(endFunction) lol();
+      return clearInterval(interval);
+    }
+    toText.data		+=	from.substr(0, charAmount);
+    fromText.data	=	from.substr(charAmount);
+
+    if(autoScroll && ($('.terminalwrapper').get(0).scrollTop > 20))
+      console.log($('.terminalwrapper').get(0).scrollTop)
+    $('.terminalwrapper').animate({
+      scrollTop: $('.terminalwrapper').get(0).scrollHeight}, 0);
+
+
+
+  }, refreshRate);
+  return interval;
+}
+
+stringData = "\n kevin@murphy ~ $ sudo su \n [sudo] password for kevin:       \n murphy # ./kevin.run\n Loading Website...."
+
+stringData2 = "\n\n\n\n ERROR \n CRITICAL ERROR. \n ABORT ABORT. \n WHY IS THIS HAPPENING? \n NEVER GONNA GIVE YOU UP\n NEVER GONNA LET YOU DOWN......"
+
+
+
+snakeText(document.createTextNode(stringData), terminalElement, 53, 2, true, true);
+
+function lol(){
+  $('#rick').show();
+  $("#player")[0].src += "?autoplay=1";
+  $("#Title").text("I'M SORRY");
+  $("#Subtitle").text("THIS WASN'T SUPPOSED TO HAPPEN");
+  snakeText(document.createTextNode(stringData2), terminalElement, 455, 1, true, false);
+}
+
